66a380452a5e24da1e707e254653a34d4329cec8
denis.nsc <denis.nsc@b8f5f8ae-a533-11de-b950-3d5b5f4ea0aa>
2009-12-13 21:34:54 +0000

    Added Fist-Fit and greedy modification of Next-Fit and First-Fit

--- trunk/src/com/googlecode/caparf/examples/SimpleFitTestScenario.java
+++ trunk/src/com/googlecode/caparf/examples/SimpleFitTestScenario.java
@@ -1,6 +1,8 @@
 package com.googlecode.caparf.examples;
 
-import com.googlecode.caparf.algorithms.spp2d.NextFit;
+import com.googlecode.caparf.algorithms.spp2d.SimpleFit;
+import com.googlecode.caparf.algorithms.spp2d.SimpleFit.ItemOrder;
+import com.googlecode.caparf.algorithms.spp2d.SimpleFit.PlacementStrategy;
 import com.googlecode.caparf.framework.AlgorithmInputReader;
 import com.googlecode.caparf.framework.AlgorithmRunner;
 import com.googlecode.caparf.framework.spp2d.Input;
@@ -9,13 +11,18 @@ import com.googlecode.caparf.framework.spp2d.OutputVerifier;
 import com.googlecode.caparf.tests.spp2d.BerkeyWangGenerator;
 import com.googlecode.caparf.tests.spp2d.BerkeyWangGenerator.Type;
 
-public class NextFitTestScenario {
+public class SimpleFitTestScenario {
 
   public static void main(String[] args) {
-    NextFit algorithm = new NextFit();
-    
+    SimpleFit nextFit = new SimpleFit(ItemOrder.NEXT_ITEM, PlacementStrategy.DEFAULT);
+    SimpleFit greedyNextFit = new SimpleFit(ItemOrder.NEXT_ITEM,
+        PlacementStrategy.SHIFT_RIGHTMOST_ITEM);
+    SimpleFit firstFit = new SimpleFit(ItemOrder.FIRST_FIT, PlacementStrategy.DEFAULT);
+    SimpleFit greedyFirstFit = new SimpleFit(ItemOrder.FIRST_FIT,
+        PlacementStrategy.SHIFT_RIGHTMOST_ITEM);
+
     OutputVerifier verifier = new OutputVerifier();
-    
+
     AlgorithmInputReader<Input> inputReader = new InputReader()
         .addAll(BerkeyWangGenerator.getReferenceInstances(Type.CLASS_I))
         .addAll(BerkeyWangGenerator.getReferenceInstances(Type.CLASS_II))
@@ -23,7 +30,10 @@ public class NextFitTestScenario {
         .addAll(BerkeyWangGenerator.getReferenceInstances(Type.CLASS_IV))
         .addAll(BerkeyWangGenerator.getReferenceInstances(Type.CLASS_V))
         .addAll(BerkeyWangGenerator.getReferenceInstances(Type.CLASS_VI));
-    
-    AlgorithmRunner.run(inputReader, algorithm, verifier);
+
+    AlgorithmRunner.run(inputReader, nextFit, verifier);
+    AlgorithmRunner.run(inputReader, greedyNextFit, verifier);
+    AlgorithmRunner.run(inputReader, firstFit, verifier);
+    AlgorithmRunner.run(inputReader, greedyFirstFit, verifier);
   }
 }
