59cb62eee8fe8f55b10658af47e1689bc5895e0d
denis.nsc <denis.nsc@b8f5f8ae-a533-11de-b950-3d5b5f4ea0aa>
2010-01-17 21:03:47 +0000

    Add copyright notice to all source files.

--- trunk/src/com/googlecode/caparf/framework/runner/TextListener.java
+++ trunk/src/com/googlecode/caparf/framework/runner/TextListener.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2010 Denis Nazarov <denis.nsc@gmail.com>.
+ *
+ * This file is part of caparf (http://code.google.com/p/caparf/).
+ *
+ * caparf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * caparf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with caparf. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.googlecode.caparf.framework.runner;
 
 import com.googlecode.caparf.framework.base.Algorithm;
